07cc1f67eb00de04f5307b14d647b61acf79cadf
Jonathan Ruiz <jonathan.ruiz@rivetlogic.com>
2015-04-17 01:02:37 -0600

    Continue button fix.

--- Assets/Scripts/ULTIMATE/General/ContinueButton.cs
+++ Assets/Scripts/ULTIMATE/General/ContinueButton.cs
@@ -9,14 +9,14 @@ public class ContinueButton : MonoBehaviour {
 	void Start () {
 		continueBtn = GameObject.FindGameObjectWithTag ("ContinueButton");
 		if (continueBtn != null) {
+			Debug.Log(PlayerPrefs.GetInt("LevelName"));
+			Debug.Log(PlayerPrefs.GetFloat("PlayerX"));
 			if(PlayerPrefs.GetInt("LevelName") < 1 && PlayerPrefs.GetFloat("PlayerX") <= 0 ){
 				continueBtn.SetActive(false);
 			}else {
 				level = PlayerPrefs.GetInt("LevelName") ;
 			}
-		
 		}
-
 	}
 	
 	// Update is called once per frame
