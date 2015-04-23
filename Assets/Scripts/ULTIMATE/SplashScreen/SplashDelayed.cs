f75cebffdb3917f7370e9e6aea15f6b74c9ec025
gob94 <gob94@outlook.com>
2015-04-22 22:49:36 -0600

    splash e historia en imagenes

---
+++ Assets/Scripts/ULTIMATE/SplashScreen/SplashDelayed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashDelayed : MonoBehaviour {
+
+	public float delayTime = 5;
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(delayTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Application.LoadLevel (1);
+	}
+}
---
+++ Assets/Scripts/ULTIMATE/SplashScreen/SplashScreenLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenLoader : MonoBehaviour {
+	public float delayTime = 5;
+	public bool done = false;
+
+	private float timer;
+	// Use this for initialization
+	void Start () {
+		timer = delayTime;
+
+		StartCoroutine ("SomeFuction");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer > 0)
+			return;
+
+		if (done)
+			Application.LoadLevel (1);
+
+	}
+
+	IEnumerator SomeFuction(){
+		yield return null;
+
+		done = true;
+
+	}
+}
